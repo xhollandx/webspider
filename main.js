f376a3eb41dc772b8518f7bb9f3227d0650d60d5
xhollandx <rtf2225i@gmail.com>
2025-06-02 12:46:35 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onland = function() {
+var reply = promt("bienvenid@ ¿cual es tu nombre?", " ")
+alert ( "es un gusto tenerte aqui " + reply + " ! ")
+}
+let miBoton = document.querySelector("button");
+miBoton.onclick = fuction() {
+alert("con el codigo: JT05 obtendrás un 5 % en tu próxima compra");
+}
